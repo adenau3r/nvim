e461bc32cb1c8c16a2ade203c781a2a9d822d353
shiikato <Conreg@benedikt-Fischer.net>
2024-02-10 16:22:01 +0100

    Changed theme number

--- lua/conradf/lazy.lua
+++ lua/conradf/lazy.lua
@@ -1,3 +1,5 @@
+themeNumber = 3
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
@@ -56,5 +58,4 @@ end
 opts = {}
 require("lazy").setup(plugins, opts)
 
-currTheme = themes[3]
-vim.cmd.colorscheme(currTheme[2])
+vim.cmd.colorscheme(themes[themeNumber][2])
