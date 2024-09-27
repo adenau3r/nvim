cf757247ed5f8747b9f3a72f508f68367391516f
Conradf <conradf.dev@gmail.com>
2024-09-27 11:22:33 +0200

    Add nvim-tmux navigation.

---
+++ lua/plugins/tmux.lua
@@ -0,0 +1,16 @@
+return {
+    "alexghergh/nvim-tmux-navigation",
+    keys = {
+        "<C-h>",
+        "<C-j>",
+        "<C-k>",
+        "<C-l>"
+    },
+    config = function()
+        require("nvim-tmux-navigation").setup({})
+        vim.keymap.set("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>")
+        vim.keymap.set("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>")
+        vim.keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>")
+        vim.keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>")
+    end
+}
